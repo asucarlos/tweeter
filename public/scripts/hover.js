ced61caca46d177714a044eb72846767e50a032a
vagrant <kuwahara.mee@gmail.com>
2019-06-12 16:42:28 +0000

    hover function added

---
+++ public/scripts/hover.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".news-post").hover(function () {
+    $(this).css("border", "1px solid black");
+    $(this).children(".news-post-header").css("opacity", "1");
+    $(this).children(".news-post-footer").children(".icons").css("opacity", "1");
+    }
+
+    , function(){
+      $(this).css("border", "1px solid rgba(0, 0, 0, 0.3)")
+      $(this).children(".news-post-header").css("opacity", "0.5");
+      $(this).children(".news-post-footer").children(".icons").css("opacity", "0");
+      })
+})
