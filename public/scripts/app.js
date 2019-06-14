5d116c312df685d3debd72ef9e2eea638a17c813
vagrant <kuwahara.mee@gmail.com>
2019-06-14 17:33:10 +0000

    delete unnecesary file and comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-// Test / driver code (temporary). Eventually will get this from the server.
-
-//temporary tweet database
+//tweet database
 const data =[];
 
 // render the tweets to the timeline
