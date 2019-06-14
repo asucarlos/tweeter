c03227985dbfc5c0102f5f0fd6fff462b3af4061
vagrant <kuwahara.mee@gmail.com>
2019-06-14 17:11:00 +0000

    delete server side data unnecesary comments

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,30 +1,23 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-// const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Save a tweet to `db`
     saveTweet: function(newTweet, callback) {
         db.collection("tweets").insertOne(newTweet)
         callback(null, true);
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `db`
     getTweets: function(callback) {
         db.collection("tweets").find().toArray((err, tweets) => {
       if (err) {
         return callback(err);
       }
       callback(null, tweets);
-    });
-
-
-        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        // callback(null, db.tweets.sort(sortNewestFirst));
+     });
     }
 
   };
